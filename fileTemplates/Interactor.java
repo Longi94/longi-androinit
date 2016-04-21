0151438c83e1a9736a39151fa41e9fc83cc83e80
Long Tran <lngtrn94@gmail.com>
2016-04-21 21:35:10 +0200

    move templates to another repository

--- fileTemplates/Interactor.java
+++
@@ -1,9 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-public interface ${NAME}Interactor extends Interactor {
-    interface Callback {
-        void on${NAME}Finish();
-
-        void on${NAME}Fail();
-    }
-}
